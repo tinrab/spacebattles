b4447847d9193550249822639d7c6908db997c06
paidgeek <tin.rabzelj@gmail.com>
2016-11-18 06:24:32 +0100

    menu layout

---
+++ Assets/Editor/LocalizeAll.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class LocalizeAll : MonoBehaviour
+{
+  [MenuItem("Tools/Localize All")]
+  public static void Perform()
+  {
+    var texts = FindObjectsOfType<LocalizeText>();
+
+    for (var i = 0; i < texts.Length; i++) {
+      texts[i].Localize();
+    }
+  }
+}
